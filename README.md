8043cec0d1f388862b277e044a349c3a3b02fd1c
stolyarmishka1 <63854139+stolyarmishka1@users.noreply.github.com>
2020-05-09 01:25:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CPPArcade
+
+Developed with Unreal Engine 4
---
+++ Source/CPPArcade/CPPArcadeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPPArcadeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPARCADE_API ACPPArcadeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
